72f83c931cab2813a59d8b8311906ea53f275cb8
money <hualigushi@163.com>
2020-01-15 16:12:09 +0800

    Update user.js

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,34 +1,21 @@
-/** 
- * user api 路由
+/**
+ * @description user API 路由
+ * @author jiuchang
  */
 
-const router = require('koa-router')()
-const {
-    isExist,
-    register,
-    login,
-    changeInfo,
-    changePassword,
-    logout
-} = require('../../controller/user')
-const userValidate = require('../../validator/user')
-const {
-    genValidator
-} = require('../../middlewares/validator')
-const {
-    loginCheck
-} = require('../../middlewares/loginChecks')
+const router=require('koa-router')()
+const {isExist,register,login,deleteCurUser,changeInfo,changePassword,logout} =require('../../controller/user')
+const userValidate=require('../../validator/user')
+const {genValidator} =require('../../middlewares/validator')
+const {loginCheck}=require('../../middlewares/loginChecks')
+const {isTest} =require('../../utils/env')
 
 router.prefix('/api/user')
 
-//注册
-router.post('/register', genValidator(userValidate), async (ctx, next) => {
-    const {
-        userName,
-        password,
-        gender
-    } = ctx.request.body
-    ctx.body = await register({
+//注册路由
+router.post('/register',genValidator(userValidate),async (ctx,next)=>{
+    const {userName,password,gender}=ctx.request.body
+    ctx.body=await register({
         userName,
         password,
         gender
@@ -36,58 +23,46 @@ router.post('/register', genValidator(userValidate), async (ctx, next) => {
 })
 
 
-//用户名是否存在
-router.post('/isExist', async (ctx, next) => {
-    const {
-        userName
-    } = ctx.request.body
 
+//注册路由
+router.post('/isExist',async (ctx,next)=>{
+    const {userName}=ctx.request.body
     //controller
-    ctx.body = await isExist(userName)
-
+    ctx.body=await isExist(userName)
 })
-
 
 //登录
-router.post('/login', async (ctx, next) => {
-    const {
-        userName,
-        password
-    } = ctx.request.body
-    ctx.body = await login(ctx, userName, password)
+router.post('/login',async (ctx,next)=>{
+    const {userName,password}=ctx.request.body
+    ctx.body=await login(ctx,userName,password)
 })
 
-//修改信息
-router.patch('/changeInfo', loginCheck, genValidator(userValidate), async (ctx, next) => {
-    const {
-        nickName,
-        city,
-        picture
-    } = ctx.request.body
+//删除
+router.post('/delete',loginCheck,async (ctx,next)=>{
+    if(isTest){
+        //测试环境下，测试账号登录之后，删除自己
+        const {userName} =ctx.session.userInfo
+        ctx.body=await deleteCurUser(userName)
+    }
 
-    ctx.body = await changeInfo(ctx, {
-        nickName,
-        city,
-        picture
-    })
+})
+
+//修改个人信息
+router.patch('/changeInfo',loginCheck,genValidator(userValidate),async (ctx,next)=>{
+    const {nickName,city,picture}=ctx.request.body
+    ctx.body=await changeInfo(ctx,{nickName,city,picture})
 })
 
 //修改密码
-router.patch('/changePassword', loginCheck, genValidator(userValidate), async (ctx, next) => {
-    const {
-        password,
-        newPassword
-    } = ctx.request.body
-    const {
-        userName
-    } = ctx.session.userInfo
-    ctx.body = await changePassword(userName, password, newPassword)
+router.patch('/changePassword',loginCheck,genValidator(userValidate), async (ctx,next)=>{
+    const {password,newPassword}=ctx.request.body
+    const {userName}=ctx.session.userInfo
+    ctx.body=await changePassword(userName,password,newPassword)
 })
 
-//退出登录
-router.post('/logout', loginCheck, async (ctx, next) => {
-    ctx.body = await logout(ctx)
+//退出登录 
+router.post('/logout',loginCheck,async (ctx,next)=>{
+    ctx.body=await logout(ctx)
 })
 
-
-module.exports = router
+module.exports=router
