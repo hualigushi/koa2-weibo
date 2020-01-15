a9d81e58ffda65cc93432356c8ec962ce13e89b6
money <hualigushi@163.com>
2020-01-15 16:14:50 +0800

    Update user.js

--- src/controller/user.js
+++ src/controller/user.js
@@ -16,6 +16,7 @@ const {
     registerUserNameNotExistInfo,
     registerFailInfo,
     loginFailInfo,
+    deleteUserFailInfo,
     changeInfoFailInfo,
     changePasswordFailInfo
 } = require('../model/ErrorInfo')
@@ -88,6 +89,20 @@ async function login(ctx, userName, password) {
         ctx.session.userInfo = userInfo
     }
     return new SuccessModel()
+}
+
+/**
+ * 删除当前用户
+ * @param {string} userName 用户名 
+ */
+async function deleteCurUser(userName){
+    const result=await deleteUser(userName)
+    if(result){
+        //成功
+        return new SuccessModel()
+    }
+    //失败
+    return new ErrorModel(deleteUserFailInfo)
 }
 
 //修改个人信息
