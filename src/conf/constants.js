5af991cc90a90324d0a8eeb53af1bb644581faaf
money <hualigushi@163.com>
2020-01-16 11:54:54 +0800

    Update constants.js

--- src/conf/constants.js
+++ src/conf/constants.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-    DEFASULT_PICTURE: 'https://dwz.cn/rnTnftZs',
+    DEFAULT_PICTURE: 'https://dwz.cn/rnTnftZs',
     PAGE_SIZE: 5,
     // 正则匹配‘@昵称 - userName’
     REG_FOR_AT_WHO: /@(.+?)\s-\s(\w+?)\b/g
