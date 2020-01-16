816815a05bed8d2bccc0201b6e7cebe608352333
money <hualigushi@163.com>
2020-01-16 10:07:03 +0800

    Update blog-home.js

--- src/controller/blog-home.js
+++ src/controller/blog-home.js
@@ -1,43 +1,62 @@
-/** 
- * 首页 controller
+/**
+ * @description 首页 
+ * @author
  */
 
-const xss = require('xss')
-const {
-    SuccessModel,
-    ErrorModel
-} = require('../model/ResModel')
-const {
-    createBlogFailInfo
-} = require('../model/ErrorInfo')
+const {createBlog,getFollowersBlogList}=require('../services/blog')
+const {SuccessModel,ErrorModel}=require('../model/ResModel')
+const { PAGE_SIZE, REG_FOR_AT_WHO } = require('../conf/constant')
+const {createBlogFailInfo}=require('../model/ErrorInfo')
+const xss=require('xss')
 
-const {
-    createBlog
-} = require('../services/blog')
-
-
-/** 
+/**
  * 创建微博
- */
-async function create({
-    userId,
-    content,
-    image
-}) {
+ * @param {Object} param0 创建微博所需的数据 {userId,content,image}
+ */ 
+async function create({userId,content,image}){
+    // 创建要try-catch
     try {
         //创建微博
-        const blog = await createBlog({
+        const blog=await createBlog({
             userId,
             content: xss(content),
             image
         })
         return new SuccessModel(blog)
-    } catch (ex) {
-        console.error(ex.message, ex.stack)
+    } catch (error) {
+        console.error(error.message,ex.stack)
         return new ErrorModel(createBlogFailInfo)
     }
+
 }
 
-module.exports = {
-    create
+/**
+ * 获取首页微博列表
+ * @param {number} userId userId
+ * @param {number} pageIndex page index
+ */
+async function getHomeBlogList(userId, pageIndex = 0) {
+    const result = await getFollowersBlogList(
+        {
+            userId,
+            pageIndex,
+            pageSize: PAGE_SIZE
+        }
+    )
+    const { count, blogList } = result
+
+    // 返回
+    return new SuccessModel({
+        isEmpty: blogList.length === 0,
+        blogList,
+        pageSize: PAGE_SIZE,
+        pageIndex,
+        count
+    })
+}
+
+
+module.exports={
+    create,
+    getHomeBlogList
 }
