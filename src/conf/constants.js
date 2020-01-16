b96a39063ca8da61f1e2b68ace1d2d0fe7d49da6
money <hualigushi@163.com>
2020-01-16 11:12:31 +0800

    Update constants.js

--- src/conf/constants.js
+++ src/conf/constants.js
@@ -5,6 +5,8 @@
 module.exports = {
     SECRET: 'UsdU*976_+qwe',
     DEFASULT_PICTURE: 'https://dwz.cn/rnTnftZs',
-    PAGE_SIZE: 5
+    PAGE_SIZE: 5,
+    // 正则匹配‘@昵称 - userName’
+    REG_FOR_AT_WHO: /@(.+?)\s-\s(\w+?)\b/g
 
 }
