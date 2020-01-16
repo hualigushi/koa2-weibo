bc76eb2665cc46e490f63fe0bad25844097aafa2
money <hualigushi@163.com>
2020-01-16 10:17:14 +0800

    Update index.js

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -1,28 +1,37 @@
-/** 
- * 数据模型入口文件
+/**
+ * @description 数据模型入口文件
+ * @author jiuchang
  */
 
-const User = require('./User')
-const Blog = require('./Blog')
-const UserRelation = require('./UserRelation')
+const User=require('./User')
+const Blog=require('./Blog')
+const UserRelation=require('./UserRelation')
+const AtRelation = require('./AtRelation')
 
-//创建外键
-//查询微博顺便查出用户信息
-Blog.belongsTo(User, {
+Blog.belongsTo(User,{
     foreignKey: 'userId'
 })
 
-UserRelation.belongsTo(User, {
+UserRelation.belongsTo(User,{
     foreignKey: 'followerId'
 })
 
-User.hasMany(UserRelation, {
-    foreignKey: 'userId'
+User.hasMany(UserRelation,{
+    foreignKey:'userId'
 })
 
+Blog.belongsTo(UserRelation, {
+    foreignKey: 'userId',
+    targetKey: 'followerId'
+})
 
-module.exports = {
+Blog.hasMany(AtRelation, {
+    foreignKey: 'blogId'
+})
+
+module.exports={
     User,
     Blog,
-    UserRelation
+    UserRelation,
+    AtRelation
 }
