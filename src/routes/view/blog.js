0c2ddd978c3adc885417c92d1ebefd80517ef467
money <hualigushi@163.com>
2020-01-16 09:57:13 +0800

    Update blog.js

--- src/routes/view/blog.js
+++ src/routes/view/blog.js
@@ -1,84 +1,97 @@
-/** 
- * 微博 view 路由
+/**
+ * @description 微博 view 路由
+ * @author jiuchang
  */
 
-const router = require('koa-router')()
-const {
-    loginRedirect
-} = require('../../middlewares/loginChecks')
-const {
-    getProfileBlogList
-} = require('../../controller/blog-profile')
-const {
-    getSquareBlogList
-} = require('../../controller/blog-square')
-const {
-    getFans,
-    getFollowers
-} = require('../../controller/user-relation')
-const {
-    isExist
-} = require('../../controller/user')
+const router=require('koa-router')()
+const {loginRedirect}=require('../../middlewares/loginChecks')
+const { getHomeBlogList } = require('../../controller/blog-home')
+const { getSquareBlogList } = require('../../controller/blog-square')
+const { getFans, getFollowers } = require('../../controller/user-relation')
+const {getProfileBlogList} =require('../../controller/blog-profile')
+const { getAtMeCount, getAtMeBlogList, markAsRead } = require('../../controller/blog-at')
 
 //首页
 router.get('/', loginRedirect, async (ctx, next) => {
-    await ctx.render('index', {})
+    const userInfo = ctx.session.userInfo
+    const { id: userId } = userInfo
+    // 获取第一页数据
+    const result = await getHomeBlogList(userId)
+    const { isEmpty, blogList, pageSize, pageIndex, count } = result.data
+
+    // 获取粉丝
+    const fansResult = await getFans(userId)
+    const { count: fansCount, fansList } = fansResult.data
+
+    // 获取关注人列表
+    const followersResult = await getFollowers(userId)
+    const { count: followersCount, followersList } = followersResult.data
+
+    // 获取 @ 数量
+    const atCountResult = await getAtMeCount(userId)
+    const { count: atCount } = atCountResult.data
+
+    await ctx.render('index', {
+        userData: {
+            userInfo,
+            fansData: {
+                count: fansCount,
+                list: fansList
+            },
+            followersData: {
+                count: followersCount,
+                list: followersList
+            },
+            atCount
+        },
+        blogData: {
+            isEmpty,
+            blogList,
+            pageSize,
+            pageIndex,
+            count
+        }
+    })
 })
 
 //个人主页
-router.get('/profile', loginRedirect, async (ctx, next) => {
-    const {
-        userName
-    } = ctx.session.userInfo
+router.get('/profile',loginRedirect,async (ctx,next)=>{
+    const {userName}=ctx.session.userInfo
     ctx.redirect(`/profile/${userName}`)
 })
-router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
-    // 已登录用户的信息
-    const myUserInfo = ctx.session.userInfo
-    const myUserName = myUserInfo.userName
+router.get('/profile/:userName',loginRedirect,async (ctx,next)=>{
+    //已登录用户的信息
+    const myUserInfo=ctx.session.userInfo
+    const myUserName=myUserInfo.userName
 
     let curUserInfo
-    const {
-        userName: curUserName
-    } = ctx.params
-    const isMe = myUserName === curUserName
-    if (isMe) {
-        // 是当前登录用户
-        curUserInfo = myUserInfo
-    } else {
-        // 不是当前登录用户
-        const existResult = await isExist(curUserName)
-        if (existResult.errno !== 0) {
-            // 用户名不存在
-            return
+    const {userName: curUserName}=ctx.params
+    const isMe=myUserName===curUserName
+    if(isMe){
+        //是当前登录用户
+        curUserInfo=myUserInfo
+    }else{
+        //不是当前登录用户
+        const existResult=await isExist(curUserName)
+        if(existResult.errno!==0){
+            //用户名不存在
+            return 
         }
-        // 用户名存在
-        curUserInfo = existResult.data
+        //用户名存在
+        curUserInfo=existResult.data
     }
-
-    // 获取微博第一页数据
-    const result = await getProfileBlogList(curUserName, 0)
-    const {
-        isEmpty,
-        blogList,
-        pageSize,
-        pageIndex,
-        count
-    } = result.data
+    //微博第一页数据
+    console.log('curUserName: ',curUserName)
+    const result =await getProfileBlogList(curUserName,0)
+    const {isEmpty,blogList,pageSize,pageIndex,count}=result.data
 
     // 获取粉丝
     const fansResult = await getFans(curUserInfo.id)
-    const {
-        count: fansCount,
-        fansList
-    } = fansResult.data
+    const { count: fansCount, fansList } = fansResult.data
 
     // 获取关注人列表
     const followersResult = await getFollowers(curUserInfo.id)
-    const {
-        count: followersCount,
-        followersList
-    } = followersResult.data
+    const { count: followersCount, followersList } = followersResult.data
 
     // 我是否关注了此人？
     const amIFollowed = fansList.some(item => {
@@ -87,11 +100,10 @@ router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
 
     // 获取 @ 数量
     const atCountResult = await getAtMeCount(myUserInfo.id)
-    const {
-        count: atCount
-    } = atCountResult.data
+    const { count: atCount } = atCountResult.data
 
-    await ctx.render('profile', {
+
+    await ctx.render('profile',{
         blogData: {
             isEmpty,
             blogList,
@@ -116,18 +128,11 @@ router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
     })
 })
 
-
 // 广场
 router.get('/square', loginRedirect, async (ctx, next) => {
     // 获取微博数据，第一页
     const result = await getSquareBlogList(0)
-    const {
-        isEmpty,
-        blogList,
-        pageSize,
-        pageIndex,
-        count
-    } = result.data || {}
+    const { isEmpty, blogList, pageSize, pageIndex, count } = result.data || {}
 
     await ctx.render('square', {
         blogData: {
@@ -140,4 +145,4 @@ router.get('/square', loginRedirect, async (ctx, next) => {
     })
 })
 
-module.exports = router
+module.exports=router
